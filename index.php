49e13737bd5e8f139879122395bd0aa2215114fe
f-mkotsovoulou <!CloudGit2020#>
2021-03-23 18:02:49 +0200

    sessions

--- index.php
+++ index.php
@@ -34,10 +34,13 @@ session_start();?>
                         <li><a class="dropdown-item" href="#">Tops</a></li>
                     </ul>
                 </li>
-
+                <?php
+                if (isset($_SESSION["isAdmin"]) && $_SESSION["isAdmin"] == 1) {
+                ?>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">ADMIN</a>
+                    <a class="nav-link" href="admin.php">ADMIN</a>
                 </li>
+                <?php } ?>
 
             </ul>
         </div>
@@ -45,18 +48,46 @@ session_start();?>
 </nav>
 
 
-<?php
-if (isset($_SESSION["email"]) )
-echo 'Welcome ' . $_SESSION["email"];
 
-if ( isset($_SESSION["isAdmin"]) && $_SESSION["isAdmin"]==1 ) { ?>
-<a href="admin.php"> Click here to administer users</a>
+<br>
+<?php
+if (!isset($_SESSION["email"]) ) { ?>
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#loginModal">
+    Login
+</button>
+<?php } else { ?>
+ <h1> You are logged in as : <?php echo $_SESSION["email"]; ?></h1>
+ <a href="logout.php" class="btn btn-secondary"> Logout </a>
 <?php } ?>
 
-<a class="btn btn-primary" href="login.php" role="button">Login</a>
+<!-- Modal -->
+<div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Login</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form method="POST" action="dologin.php">
+            <div class="modal-body">
 
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+                    <input type="email" id="email" name="email" required placeholder="Email"> </br>
+                    <input type="password" id="password" name="password" placeholder="password" required> </br>
 
+            </div>
+            <div class="modal-footer">
+                <button type="submit" class="btn btn-primary">Login</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 </body>
 </html>
